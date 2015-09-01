711c89539f0d6f87f3bd67ccba319303a2ad76b7
David Muegge <david@muegge.com>
2015-09-01 09:16:39 -0400

    Folder Structure Change

---
+++ DMGitTest.psm1
@@ -0,0 +1,39 @@
+﻿		<# Module Name:     DMGitTest.psm1
+		## 
+		## Author:          David Muegge
+		##
+		## Purpose:         
+		##																					
+		## Requirements:    PowerShell Version 3
+		## 										
+		## History:
+		##                  0.10 2013-03-01 - Initial Creation
+		##                                  
+		##                                                                             
+		###################################################################################################>
+			
+		# Helper functions
+			
+		
+		# Exported Functions
+		
+		function Get-DMGitTest{
+		<#
+			.Synopsis
+				Test
+		
+			.Description
+				Test
+		
+		#>
+		[CmdletBinding()]
+		Param()
+		
+		    Write-Host "DMGitTest"
+		
+		}
+		
+		
+							
+		
+						
